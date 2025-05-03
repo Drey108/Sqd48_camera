8a790b3b09800f8fc0226f97beb0725373fe330e
Drey108 <92579304+Drey108@users.noreply.github.com>
2025-05-03 14:44:09 +0530

    fixed url error in all urls in frontend

--- frontend/camera/src/components/Sell.jsx
+++ frontend/camera/src/components/Sell.jsx
@@ -70,7 +70,7 @@ const dropdownmenu = async(value) =>{
 
   const handleUpdate = async (cameraId, updatedData) => {
     try {
-      const response = await axios.put(`${import.meta.env.VITE_API_URL}/sell-cameras/${cameraId}`, updatedData,{ withCredentials: true });
+      const response = await axios.put(`${import.meta.env.VITE_API_URL}/api/sell-cameras/${cameraId}`, updatedData,{ withCredentials: true });
       if (response.status === 200) {
         console.log('Camera updated successfully');
         fetchSellCameras();
